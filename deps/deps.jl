2ee187720615940cbc83ee68884e7925590d2a56
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-04-22 16:06:58 +0530

    Don't ignore deps.jl

---
+++ deps/deps.jl
@@ -0,0 +1,8 @@
+const python = "/home/jrun/.julia/artifacts/4dcd8a52e092c1e82cef8e0c0ed138b30c4cd17b/bin/python3"
+const libpython = "/home/jrun/.julia/artifacts/4dcd8a52e092c1e82cef8e0c0ed138b30c4cd17b/lib/libpython3.8.so.1.0"
+const pyprogramname = "/home/jrun/.julia/artifacts/4dcd8a52e092c1e82cef8e0c0ed138b30c4cd17b/bin/python3"
+const pyversion_build = v"3.8.1"
+const PYTHONHOME = "/home/jrun/.julia/artifacts/4dcd8a52e092c1e82cef8e0c0ed138b30c4cd17b:/home/jrun/.julia/artifacts/4dcd8a52e092c1e82cef8e0c0ed138b30c4cd17b"
+
+"True if we are using the Python distribution in the Conda package."
+const conda = false
